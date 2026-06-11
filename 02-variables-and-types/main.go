59d35055369e37dc73580975e36d7090fcad1c1a
Miguel Ángel <maacostaw3@gmail.com>
2026-06-10 21:24:17 -0500

    review part 2

--- 02-variables-and-types/main.go
+++ 02-variables-and-types/main.go
@@ -4,11 +4,11 @@ import "fmt"
 
 func main() {
 
-	// <var> <name> <type>
-	var channelName string;
+	// var <name> <type>
+	var channelName string
 	channelName = "Test Channel"
 
-	// <var> <name> <type> = <value>
+	// var <name> <type> = <value>
 	var year int = 2026
 
 	fmt.Println("Channel:", channelName)
--- 11-for-classic-loop/main.go
+++ 11-for-classic-loop/main.go
@@ -10,4 +10,13 @@ func main() {
 	for i := 1; i <= 5; i++ {
 		fmt.Println(i)
 	}
+
+	N := 10
+	sum := 0
+
+	for i := 1; i <= N; i++ {
+		sum = sum + i
+	}
+
+	fmt.Println(sum)
 }
